3c498250eeed97afd4ef42d6f7f50aabbb0d4b87
Nate Stott <nate.stott@pm.me>
2025-11-05 08:51:27 -0700

    clean up client

--- packages/client/packages/server/repositories/product.repository.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '../prisma/client';
-
-export const productRepository = {
-    async getProduct(productId: number) {
-        return prisma.product.findUnique({
-            where: { id: productId },
-        });
-    },
-};
